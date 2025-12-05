6c1bb8a7d2602c56fca090ae7bd3db26b49e674e
CHRISLeGURU <salumuchris21@gmail.com>
2025-12-05 13:04:26 +0200

    ssr installation

--- lib/supabaseBrowserClient.ts
+++ lib/supabaseBrowserClient.ts
@@ -1,18 +1,13 @@
 "use client";
 
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error(
-    "Les variables NEXT_PUBLIC_SUPABASE_URL et NEXT_PUBLIC_SUPABASE_ANON_KEY doivent être définies."
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
 
-export const supabaseBrowserClient = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+export const supabaseBrowserClient = createClient();
 
